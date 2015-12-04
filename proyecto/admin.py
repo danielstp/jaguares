7eef6f0d830534ca6e36b181339286f512441b97
Daniel Saguez Tezanos Pinto <danielstp@gmail.com>
2015-12-04 16:22:50 -0400

    Añadi las clases del modelo

--- proyecto/admin.py
+++ proyecto/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from proyecto.models import Documento
+from proyecto.models import Proyecto
+from proyecto.models import Miembro
+from proyecto.models import Persona
+from proyecto.models import HistoriaUsuario
+from proyecto.models import HistoriaTarea
+from proyecto.models import Tarea
+from proyecto.models import Rol
+from proyecto.models import CriterioAceptacion
+from proyecto.models import Sprint
+from proyecto.models import EstadoEmocional
+from proyecto.models import Estado
+
+
+admin.site.register(Documento)
+admin.site.register(Proyecto)
+admin.site.register(Miembro)
+admin.site.register(Persona)
+admin.site.register(HistoriaUsuario)
+admin.site.register(HistoriaTarea)
+admin.site.register(Tarea)
+admin.site.register(Rol)
+admin.site.register(CriterioAceptacion)
+admin.site.register(Sprint)
+admin.site.register(EstadoEmocional)
+admin.site.register(Estado)
--- proyecto/models.py
+++ proyecto/models.py
@@ -1,26 +1,77 @@
 from django.db                import models
 from django.utils.translation import ugettext_lazy as _
 from django.core.validators   import RegexValidator
+from datetime                 import datetime
+
+
+class HistoriaUsuario(models.Model):
+    def __unicode__(self):
+        return self.nombre
+
+
+class HistoriaTarea(models.Model):
+    def __unicode__(self):
+        return self.nombre
+
+
+class Tarea(models.Model):
+    def __unicode__(self):
+        return self.nombre
+
+
+class Rol(models.Model):
+    def __unicode__(self):
+        return self.nombre
+
+
+class CriterioAceptacion(models.Model):
+    def __unicode__(self):
+        return self.nombre
+
+
+class Sprint(models.Model):
+    def __unicode__(self):
+        return self.nombre
+
+
+class EstadoEmocional(models.Model):
+    def __unicode__(self):
+        return self.nombre
+
+
+class Estado(models.Model):
+    def __unicode__(self):
+        return self.nombre
 
 
 class Proyecto(models.Model):
     nombre = models.CharField(_(u'Proyecto'), max_length=250)
+    descripcion = models.CharField(_(u'Descripción'), max_length=250)
+    creado = models.DateTimeField(default=datetime.now(), editable=False)
 
     def __unicode__(self):
         return self.nombre
 
-class Miembro(models.Model):
+class Persona(models.Model):
     nombre = models.CharField(_(u'Miembro'), max_length=250)
 
     def __unicode__(self):
         return self.nombre
 
+
+class Miembro(models.Model):
+    persona = models.ForeignKey(Persona,default=)
+    proyecto = models.ForeignKey(Proyecto)
+    def __unicode__(self):
+        return self.nombre
+
+
 class Documento(models.Model):
-   nombre=models.CharField(_(u'nombre'), max_length=250)
-   descripcion=models.CharField(_(u'descripcion'), max_length=250)
-   fecha=models.DateTimeField(auto_now=True, editable=False)
-   archivo=models.FileField(upload_to=u'documentos')
-   dueno=models.ForeignKey(Miembro)
+   nombre      = models.CharField(_(u'Nombre'), max_length=250)
+   descripcion = models.CharField(_(u'Descripción'), max_length=250)
+   creado      = models.DateTimeField(auto_now=True, editable=False)
+   archivo     = models.FileField(upload_to=u'Documentos')
+   dueno       = models.ForeignKey(Miembro)
    def __unicode__(self):
      return self.nombre
 
