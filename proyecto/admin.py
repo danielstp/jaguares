9e3fc9e03efea2a8501f36cd435551cbce146be1
Daniel Saguez Tezanos Pinto <danielstp@gmail.com>
2016-02-04 00:40:42 -0500

    Añadi filtros al HU

--- proyecto/admin.py
+++ proyecto/admin.py
@@ -57,11 +57,16 @@ class SprintAdmin(admin.ModelAdmin):
     list_filter = ['nombre','proyecto']
     inlines = [ComentarioSprintInline,]
 
+
+class HistoriaUsuarioAdmin(admin.ModelAdmin):
+    list_display = ('titulo','fechaElaboracion','personaElaboro','persona')
+    list_filter = ['fechaElaboracion','personaElaboro','titulo','prioridad','persona','sprint']
+
 admin.site.register(Documento)
 admin.site.register(Proyecto, ProyectoAdmin)
 admin.site.register(Miembro)
 admin.site.register(Persona)
-admin.site.register(HistoriaUsuario)
+admin.site.register(HistoriaUsuario, HistoriaUsuarioAdmin)
 admin.site.register(HistoriaTarea)
 admin.site.register(Tarea)
 admin.site.register(Rol)
