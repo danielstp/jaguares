c1a45827d676e26ea9f811d49fa27e6d5245476b
Daniel Saguez Tezanos Pinto <danielstp@gmail.com>
2016-02-07 17:31:56 -0500

    en Tarea el documento no es obligatorio

--- proyecto/models.py
+++ proyecto/models.py
@@ -169,7 +169,7 @@ class Tarea(models.Model):
     tiempoInicioEstimado = models.DateTimeField(_(u'Fecha de Inicio (Estimado)'), default=datetime.now())
     tiempoFinalizacionEstimado = models.DateTimeField(_(u'Fecha de finalizacion (Estimado)'), default=datetime.now())
     progreso = models.DecimalField('Progreso (de 0 a 100))',default=0,max_digits=10,decimal_places=2)
-    documento = models.ForeignKey(Documento,null=True)
+    documento = models.ForeignKey(Documento,null=True,blank=True)
     miembro = models.ForeignKey(Miembro)
     estado = models.ForeignKey(Estado,default=3)
     historiaUs = models.ForeignKey(HistoriaUsuario)
